aef3f0ecc61d33b2519af2c5771cd24755a19eb9
rabu-nee <lylybui98@gmail.com>
2018-05-11 12:58:17 +0200

    all unity tutorial steps until 12.

---
+++ MultiShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float moveSpeed = 150.0f;
+    public float rotationSpeed = 3.0f;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    public float bulletSpeed = 6.0f;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * rotationSpeed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdShoot();
+        }
+    }
+
+    [Command]
+    private void CmdShoot()
+    {
+        //create the bullet
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        //add velocity to bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+
+        //Spawn bullet on clients
+        NetworkServer.Spawn(bullet);
+        
+        //destroy bullet after 2 sec
+        //TO BE CHANGED TO COLLISION
+        Destroy(bullet, 2.0f);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        //configure cameras and input
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
