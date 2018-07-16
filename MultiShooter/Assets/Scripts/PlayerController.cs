8f1eb40d2c48bb7dade83ad24ee9f10e6e8f0b8b
rabu-nee <lylybui98@gmail.com>
2018-07-16 15:10:29 +0200

    cellular automata

--- MultiShooter/Assets/Scripts/PlayerController.cs
+++ MultiShooter/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class PlayerController : NetworkBehaviour, IDamageable
-{
+public class PlayerController : NetworkBehaviour, IDamageable {
     [Header("Components")]
     [SerializeField]
     private Rigidbody rigidBody;
     [SerializeField]
     private GameObject topdownCameraPrefab;
     [SerializeField]
-    private CapsuleCollider capsuleCollider;
+    private BoxCollider capsuleCollider;
 
     [Header("Movement")]
     [SerializeField]
@@ -32,119 +31,76 @@ public class PlayerController : NetworkBehaviour, IDamageable
     private GameObject projectilePrefab;
     [SerializeField]
     private Transform projectileOrigin;
+    [SerializeField]
+    private float fireSpeed;
+    private float nextFire;
 
     private GameObject topDownCameraPivot;
     private Camera topDownCamera;
 
-    private int walkAnimationId;
+    private ObjectPooler objectPooler;
+
 
     [SyncVar]
     private float currenthealth;
 
-    private void Start()
-    {
-        if (isLocalPlayer)
-        {
+    private void Start() {
+        if (isLocalPlayer) {
             topDownCameraPivot = Instantiate(topdownCameraPrefab);
             topDownCamera = topDownCameraPivot.GetComponentInChildren<Camera>();
         }
 
+        objectPooler = ObjectPooler.Instance;
         currenthealth = maxHealth;
     }
 
-    private void Update()
-    {
-        if (isLocalPlayer)
-        {
+    private void Update() {
+        if (isLocalPlayer) {
             //CheckForProjectile();
-            if (Input.GetButton("Fire1"))
-            {
+            if (Input.GetButton("Fire1") && Time.time > nextFire) {
+                nextFire = Time.time + fireSpeed;
                 CmdFire();
             }
         }
     }
 
-    private void FixedUpdate()
-    {
-        if (isLocalPlayer)
-        {
+    private void FixedUpdate() {
+        if (isLocalPlayer) {
             Move();
         }
     }
 
-    /*
-    private void CheckForProjectile()
-    {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            RaycastHit result;
-            if (Physics.Raycast(topDownCamera.ScreenPointToRay(Input.mousePosition), out result, 100.0f, projectileLayermask))
-            {
-                Vector3 cachedPosition = projectileOrigin.transform.position;
-                Vector3 targetPosition = result.point;
-                targetPosition.y = cachedPosition.y;
-
-                //This runs on the local authority client
-                CmdRequestProjectile(cachedPosition, targetPosition - cachedPosition, gameObject);
-            }
-        }
-    }
-
     [Command]
-    private void CmdRequestProjectile(Vector3 position, Vector3 targetPosition, GameObject obj)
-    {
-        //This runs on the server
-        RpcSpawnProjectile(position, targetPosition);
-    }
-
-    [ClientRpc]
-    private void RpcSpawnProjectile(Vector3 position, Vector3 targetPosition)
-    {
-        SimpleProjectile newProjectile = Instantiate(projectilePrefab);
-        newProjectile.InitProjectile(position, targetPosition, this);
-    }
-    */
-
-    [Command]
-    void CmdFire()
-    {
+    void CmdFire() {
+        /*
         // Create the Bullet from the Bullet Prefab
         var bullet = (GameObject)Instantiate(
             projectilePrefab,
             projectileOrigin.position,
             projectileOrigin.rotation);
 
+        */
+
+        var bullet = objectPooler.SpawnFromPool("Bullet", projectileOrigin.position, projectileOrigin.rotation);
         // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        //bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 12;
 
         // Spawn the bullet on the Clients
         NetworkServer.Spawn(bullet);
-
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2.0f);
     }
 
-    IEnumerator WaitForNextBullet()
-    {
-        Debug.Log("A");
-        yield return new WaitForSeconds(5f);
-    }
-
-    private void Move()
-    {
+    private void Move() {
         Vector3 motion = Vector3.zero;
         bool bMoving = false;
 
         float horizontalMove = Input.GetAxis("Horizontal");
-        if (Mathf.Abs(horizontalMove) > 0.0f)
-        {
+        if (Mathf.Abs(horizontalMove) > 0.0f) {
             motion += topDownCamera.transform.right * horizontalMove * moveSpeed;
             bMoving = true;
         }
 
         float verticalMove = Input.GetAxis("Vertical");
-        if (Mathf.Abs(verticalMove) > 0.0f)
-        {
+        if (Mathf.Abs(verticalMove) > 0.0f) {
             Vector3 forward = topDownCamera.transform.forward;
             forward.y = 0.0f;
             forward.Normalize();
@@ -163,8 +119,7 @@ public class PlayerController : NetworkBehaviour, IDamageable
         velocity.z = motion.z;
         rigidBody.velocity = velocity;
 
-        if (bMoving)
-        {
+        if (bMoving) {
             Vector3 lookDirection = motion;
             lookDirection.y = 0.0f;
             rigidBody.MoveRotation(Quaternion.LookRotation(lookDirection, Vector3.up));
@@ -173,18 +128,15 @@ public class PlayerController : NetworkBehaviour, IDamageable
         UpdateCamera();
     }
 
-    private void UpdateCamera()
-    {
+    private void UpdateCamera() {
         topDownCameraPivot.transform.position = transform.position;
     }
 
 
-    public void Damage(float damageAmount)
-    {
+    public void Damage(float damageAmount) {
         currenthealth -= damageAmount;
 
-        if (currenthealth <= 0.0f)
-        {
+        if (currenthealth <= 0.0f) {
             enabled = false;
             capsuleCollider.gameObject.SetActive(false);
         }
